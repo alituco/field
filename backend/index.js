ac4f949b71e36661f9da575e4823b0dd8ff7c73e
Ali Altaraif <alituco@Alis-Laptop.local>
2024-08-01 00:48:45 +0300

    Added timeframes test for tfa football pitch

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { default: axios } = require('axios');
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +9,26 @@ app.use(bodyParser.json());
 
 app.get( '/', (req, res) => {
     res.send('Ello World');
+});
+
+app.post('/get-timeframes', async (req, res) => {
+    const { stadium_id, date, duration, pitch_id } = req.body;
+    try {
+        const response = await axios.post('https://api.malaebapp.com/api/v2/stadiums/timeframe', {
+            stadium_id,
+            date,
+            duration,
+            pitch_id
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        res.json(response.data);
+    } catch (err){
+        console.error('Error occured: ', err);
+        res.status(500).json({ error: 'Failed to load timeframes'});
+    }
 });
 
 app.listen(PORT, () => {
